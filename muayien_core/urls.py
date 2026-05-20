1263bc3e5c4a76dd9626d1de72007febdcaa39bf
M0𝕏 <hamoud0631@gmail.com>
2026-05-20 04:13:28 +0300

    Fix Deployment 3

--- muayien_core/urls.py
+++ muayien_core/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from api.views import RegisterView
+from api.views import RegisterView, CloseChatView, ClearHistoryView
 from api.views import ChatListView, ChatMessagesView, MeView
 
 urlpatterns = [
@@ -11,6 +11,10 @@ urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
 
     path("chats/", ChatListView.as_view(), name="list_chats"),
+
+    path("chat/<uuid:chat_id>/close/", CloseChatView.as_view(), name="close_chat"),
+    
+    path("chat/<uuid:chat_id>/clear/", ClearHistoryView.as_view(), name="clear_history"),
 
     path("chat/<uuid:chat_id>/messages/", ChatMessagesView.as_view(), name="chat_messages"),
 
